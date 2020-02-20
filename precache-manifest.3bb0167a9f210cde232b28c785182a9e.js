95ba240953b999c9b4712ed8bb9d28458f0c0e55
Mantas Miliukas <mantas@island.local>
2020-02-20 19:02:55 +0200

    Updates

---
+++ precache-manifest.3bb0167a9f210cde232b28c785182a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d22a089de74d1d1a4f9bc3a985ef3c",
+    "url": "/data/index.html"
+  },
+  {
+    "revision": "7eb0ed92606c55de488b",
+    "url": "/data/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6ceb4e07a7697eaab636",
+    "url": "/data/static/js/2.e3277f9e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/data/static/js/2.e3277f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb0ed92606c55de488b",
+    "url": "/data/static/js/main.57b2feee.chunk.js"
+  },
+  {
+    "revision": "2847af3ed59cad88b46d",
+    "url": "/data/static/js/runtime-main.d25ee62b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/data/static/media/logo.5d5d9eef.svg"
+  }
+]);
